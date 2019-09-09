913f3b6b534d06ee50cf6d73ceb35d709fb0f3b5
Jenny Sun <xmissj96@gmail.com>
2019-09-09 17:11:07 +0200

    Changes: fixed the problem. Kotlin/Java doesn't like the way I assigned the "global variables". Also added digits for the user (only T and F can be filled in). Using: android:singleLine="true" and android:imeOptions="actionNext" to still make it possible for the user to press enter.

--- app/src/main/java/com/example/jenny/logica/MainActivity.kt
+++ app/src/main/java/com/example/jenny/logica/MainActivity.kt
@@ -7,12 +7,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    // Assigns value to each input field.
-    private val firstInput = ptInput1.text.toString()
-    private val secondInput = ptInput2.text.toString()
-    private val thirdInput = ptInput3.text.toString()
-    private val fourthInput = ptInput4.text.toString()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +16,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkAnswer() {
+
+        val firstInput = ptInput1.text.toString()
+        val secondInput = ptInput2.text.toString()
+        val thirdInput = ptInput3.text.toString()
+        val fourthInput = ptInput4.text.toString()
+
         // First checks whether there is an empty input.
         if (!checkEmpty()) {
 
@@ -56,7 +56,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkEmpty() : Boolean {
+        // Assigns value to each input field.
+        val firstInput = ptInput1.text.toString()
+        val secondInput = ptInput2.text.toString()
+        val thirdInput = ptInput3.text.toString()
+        val fourthInput = ptInput4.text.toString()
+
         var empty = false
+
         // Checks whether any of the inputs are empty.
         if (firstInput.trim().isEmpty() || secondInput.trim().isEmpty() || thirdInput.trim().isEmpty() || fourthInput.trim().isEmpty()) {
             Toast.makeText(this, getString(R.string.empty), Toast.LENGTH_LONG).show()
